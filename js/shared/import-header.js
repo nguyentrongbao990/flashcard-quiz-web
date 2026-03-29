259ad7c603e3fd0d13266e1b8fe72669cb1aa2e9
nguyentrongbao990 <nguyentrongbao990@gmail.com>
2026-03-29 19:42:49 +0700

    update cấu trúc file + sửa đường dẫn

--- js/shared/import-header.js
+++ js/shared/import-header.js
@@ -2,7 +2,7 @@ document.addEventListener("DOMContentLoaded", async function () {
     const headerContainer = document.getElementById("header-container");
     if (!headerContainer) return;
 
-    const response = await fetch("header.html");
+    const response = await fetch("../components/header.html");
     const html = await response.text();
     headerContainer.innerHTML = html;
 });
